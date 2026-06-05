db66e1a4302c76be176b30baafcc66f915e1b66b
shuo-sheng <1877802914@qq.com>
2026-06-05 16:06:52 +0800

    fix: remove invalid experimental.styledJsx from next.config.js

--- next.config.js
+++ next.config.js
@@ -4,10 +4,7 @@ const nextConfig = {
   compiler: {
     styledComponents: false,
   },
-  // 禁用 styled-jsx 以解决 single-transition 语法错误
-  experimental: {
-    styledJsx: false,
-  },
+
 }
 
 module.exports = nextConfig
